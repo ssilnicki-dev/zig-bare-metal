f1cc12bcccc16f40ab1236fbf208ea2dd24a1e4b
Stanislav Silnicki <stanislav.silnicki@mailgate.us>
2024-07-14 02:07:29 +0300

    optimize build & linker scripts

--- build.zig
+++ build.zig
@@ -1,25 +1,39 @@
 const std = @import("std");
-const Target = std.Target;
 const CrossTarget = std.zig.CrossTarget;
-const Feature = std.Target.Cpu.Feature;
 
 pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSmall });
+
+    var cpu_features = std.Target.Cpu.Feature.Set.empty;
+    cpu_features.addFeature(@intFromEnum(std.Target.arm.Feature.vfp4d16sp));
+    cpu_features.addFeature(@intFromEnum(std.Target.arm.Feature.loop_align));
+    cpu_features.addFeature(@intFromEnum(std.Target.arm.Feature.no_branch_predictor));
+    cpu_features.addFeature(@intFromEnum(std.Target.arm.Feature.use_misched));
+    cpu_features.addFeature(@intFromEnum(std.Target.arm.Feature.v7em));
+
     const arm_cm4_target = CrossTarget{
         .cpu_arch = .thumb,
         .os_tag = .freestanding,
-        .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_m4 },
+        .cpu_model = .{
+            .explicit = &.{
+                .name = "cortex_m4",
+                .llvm_name = "cortex-m4",
+                .features = .{ .ints = .{0} ** 5 },
+            },
+        },
+        .cpu_features_add = cpu_features,
         .abi = .eabihf,
     };
-    const resolver_target = b.resolveTargetQuery(arm_cm4_target);
+    const target = b.resolveTargetQuery(arm_cm4_target);
 
     const elf = b.addExecutable(.{
         .name = "stm32f411.elf",
-        .target = resolver_target,
+        .target = target,
         .root_source_file = .{ .path = "src/_init.zig" },
-        .optimize = .ReleaseSafe,
+        .optimize = optimize,
     });
     const flash_init = b.addObject(.{
-        .target = resolver_target,
+        .target = target,
         .name = "flash_init",
         .root_source_file = .{ .path = "src/flash_init.zig" },
         .optimize = .ReleaseSafe,
@@ -28,30 +42,25 @@ pub fn build(b: *std.Build) void {
     elf.setLinkerScript(.{ .path = "linker.ld" });
 
     const copy_elf = b.addInstallArtifact(elf, .{});
-    const bin = b.addObjCopy(elf.getEmittedBin(), .{ .format = .bin });
-    const copy_bin = b.addInstallBinFile(bin.getOutput(), "stm32f411.bin");
-
-    bin.step.dependOn(&copy_elf.step);
-    copy_bin.step.dependOn(&bin.step);
 
     const sysram_elf = b.addExecutable(.{
         .name = "sysram-elf",
-        .target = resolver_target,
+        .target = target,
         .root_source_file = .{ .path = "src/_init.zig" },
-        .optimize = .Debug,
+        .optimize = optimize,
     });
 
     const sysram_init = b.addObject(.{
-        .target = resolver_target,
+        .target = target,
         .name = "sysram_init",
         .root_source_file = .{ .path = "src/sysram_init.zig" },
-        .optimize = .Debug,
+        .optimize = optimize,
     });
     sysram_elf.addObject(sysram_init);
     sysram_elf.setLinkerScript(.{ .path = "sysram.ld" });
 
     const copy_sram_elf = b.addInstallArtifact(sysram_elf, .{});
-    copy_sram_elf.step.dependOn(&copy_bin.step);
+    copy_sram_elf.step.dependOn(&copy_elf.step);
 
     b.default_step.dependOn(&copy_sram_elf.step);
 }
