c0094c9de244e5f97316257174407db0cb2d305c
Stanislav Silnicki <stanislav.silnicki@mailgate.us>
2024-08-01 20:41:27 +0300

    migrate zig 0.12 -> 0.14

--- build.zig
+++ build.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const CrossTarget = std.zig.CrossTarget;
 
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSmall });
@@ -11,7 +10,7 @@ pub fn build(b: *std.Build) void {
     cpu_features.addFeature(@intFromEnum(std.Target.arm.Feature.use_misched));
     cpu_features.addFeature(@intFromEnum(std.Target.arm.Feature.v7em));
 
-    const arm_cm4_target = CrossTarget{
+    const arm_cm4_target = std.Target.Query{
         .cpu_arch = .thumb,
         .os_tag = .freestanding,
         .cpu_model = .{
@@ -21,43 +20,44 @@ pub fn build(b: *std.Build) void {
                 .features = .{ .ints = .{0} ** 5 },
             },
         },
-        .cpu_features_add = cpu_features,
         .abi = .eabihf,
+        .cpu_features_add = cpu_features,
     };
+
     const target = b.resolveTargetQuery(arm_cm4_target);
 
     const elf = b.addExecutable(.{
         .name = "stm32f411.elf",
         .target = target,
-        .root_source_file = .{ .path = "src/_init.zig" },
+        .root_source_file = .{ .src_path = .{ .sub_path = "src/_init.zig", .owner = b } },
         .optimize = optimize,
     });
     const flash_init = b.addObject(.{
         .target = target,
         .name = "flash_init",
-        .root_source_file = .{ .path = "src/flash_init.zig" },
+        .root_source_file = .{ .src_path = .{ .sub_path = "src/flash_init.zig", .owner = b } },
         .optimize = .ReleaseSafe,
     });
     elf.addObject(flash_init);
-    elf.setLinkerScript(.{ .path = "linker.ld" });
+    elf.setLinkerScript(.{ .src_path = .{ .sub_path = "linker.ld", .owner = b } });
 
     const copy_elf = b.addInstallArtifact(elf, .{});
 
     const sysram_elf = b.addExecutable(.{
         .name = "sysram-elf",
         .target = target,
-        .root_source_file = .{ .path = "src/_init.zig" },
+        .root_source_file = .{ .src_path = .{ .sub_path = "src/_init.zig", .owner = b } },
         .optimize = optimize,
     });
 
     const sysram_init = b.addObject(.{
         .target = target,
         .name = "sysram_init",
-        .root_source_file = .{ .path = "src/sysram_init.zig" },
+        .root_source_file = .{ .src_path = .{ .sub_path = "src/sysram_init.zig", .owner = b } },
         .optimize = optimize,
     });
     sysram_elf.addObject(sysram_init);
-    sysram_elf.setLinkerScript(.{ .path = "sysram.ld" });
+    sysram_elf.setLinkerScript(.{ .src_path = .{ .sub_path = "sysram.ld", .owner = b } });
 
     const copy_sram_elf = b.addInstallArtifact(sysram_elf, .{});
     copy_sram_elf.step.dependOn(&copy_elf.step);
